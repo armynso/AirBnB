8ab5c33c7efd10d7278c44baaba9e4faaf3b611d
Prapassorn Tinnabavorn <Ptinnabavorn@yahoo.com>
2022-10-25 16:18:12 -0400

    Fixed firstName

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -56,14 +56,14 @@ module.exports = (sequelize, DataTypes) => {
           }
         }
       },
-      firstName: {
-        type: DataTypes.STRING,
-        // allowNull: false
-      },
-      lastName: {
-        type: DataTypes.STRING,
-        // allowNull: false
-      },
+      // firstName: {
+      //   type: DataTypes.STRING,
+      //   // allowNull: false
+      // },
+      // lastName: {
+      //   type: DataTypes.STRING,
+      //   // allowNull: false
+      // },
       email: {
         type: DataTypes.STRING,
         allowNull: false,
