7a5b2ccfeb9bf0a1a228f84858f25c92343b06c3
Prapassorn Tinnabavorn <Ptinnabavorn@yahoo.com>
2022-10-25 20:30:02 -0400

    Added seed, migration, and model for Spots

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -41,7 +41,9 @@ module.exports = (sequelize, DataTypes) => {
       return await User.scope('currentUser').findByPk(user.id);
     }
     static associate(models) {
-      // define association here
+      User.hasMany(models.Spot, {
+        foreignKey: "ownerId"
+      })
     }
   }
   User.init(
---
+++ backend/db/seeders/20221026000421-demo-spot.js
@@ -0,0 +1,49 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+     return queryInterface.bulkInsert('Spots', [
+      {
+        ownerId: 1,
+        address: "123 street",
+        city: "dc",
+        state: "virginia",
+        country: "USA",
+        lat: 27.7648569,
+        lng: -124.4731247,
+        name: "Apple",
+        description: "Place where I sleep",
+        price: 189
+      },
+      {
+        ownerId: 2,
+        address: "123 Disney Lane",
+        city: "San Francisco",
+        state: "California",
+        country: "United States of America",
+        lat: 37.7645358,
+        lng: -122.4730327,
+        name: "App Academy",
+        description: "Place where web developers are created",
+        price: 123
+      }
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete('Spots', {
+      address: { [Op.in]: ['123 street', '123 Disney Lane'] }
+    }, {});
+  }
+};
