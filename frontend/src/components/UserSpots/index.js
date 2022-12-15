54c6b713ff419f5a55917b3dba394fcbf43de660
Prapassorn Tinnabavorn <Ptinnabavorn@yahoo.com>
2022-12-15 18:37:32 -0500

    Fixed css on my spots

--- frontend/src/components/UserSpots/index.js
+++ frontend/src/components/UserSpots/index.js
@@ -38,34 +38,42 @@ function UserSpots() {
     }
 
     return (
-        <div>
+        <div className="userspots">
             <h1>My Hostings</h1>
-            {(Array.isArray(mySpots)) ? <div>
-                {mySpots.map((spot, ele) => (
-                    <tr key={ele}>
-                        <td>
-                            <img class="Image-house" src={
-                                tempImage(spot) ? spot.previewImage : 'https://images.pexels.com/photos/731812/pexels-photo-731812.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-                            }
-                                alt="No Image" width={125} height={125} />
-                        </td>
-                        <td>
-                            {spot.address}{spot.city}{spot.state}
-                        </td>
-                        <td>
-                            <div>
-                                <button onClick={() => deleteButton(spot.id)}>Delete</button>
-                            </div>
-                            <div>
-                                <NavLink to={`/mySpots/${spot.id}`}>
-                                    <button >Edit</button>
-                                    {/* onClick={() => dispatch()} */}
-                                </NavLink>
-                            </div>
-                        </td>
-                    </tr>
-                ))}
-            </div> : <div></div>}
+            <div className="userspots-flex">
+                {(Array.isArray(mySpots)) ? <div className="userspots-box">
+                    {(mySpots?.length > 0) ? (<div>
+                        {mySpots.map((spot, ele) => (
+                            <tr key={ele}>
+                                <td>
+                                    <img class="Image-house" src={
+                                        tempImage(spot) ? spot.previewImage : 'https://images.pexels.com/photos/731812/pexels-photo-731812.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                                    }
+                                        alt="No Image" width={125} height={125} />
+                                </td>
+                                <td className="userspots-address">
+                                    {spot.address} <br></br>{spot.city}, {spot.state}
+                                </td>
+                                <td>
+                                    <div>
+                                        <NavLink to={`/mySpots/${spot.id}`}>
+                                            <button >Edit</button>
+                                            {/* onClick={() => dispatch()} */}
+                                        </NavLink>
+                                    </div>
+                                    <div>
+                                        <button onClick={() => deleteButton(spot.id)}>Delete</button>
+                                    </div>
+                                </td>
+                            </tr>
+                        ))}
+                    </div>) : (
+                        <div>
+                            Your hostings will be displayed here.
+                        </div>)
+                    }
+                </div> : <div>Loading...</div>}
+            </div>
 
         </div>
     )
