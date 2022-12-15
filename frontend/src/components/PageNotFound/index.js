82a054e13ee7404b1d15befb5b176248fb284249
Prapassorn Tinnabavorn <Ptinnabavorn@yahoo.com>
2022-12-15 12:27:17 -0500

    x button

--- frontend/src/components/PageNotFound/index.js
+++ frontend/src/components/PageNotFound/index.js
@@ -4,7 +4,7 @@ function PageNotFound() {
     return (
         <div>
             <h1 className='chessh1'>
-                Page Not Found :( <br></br>Wanna watch some chess instead? :)
+                We are still working on the buttons :( <br></br>Wanna watch some chess instead? :)
             </h1>
             <div className='board'>
                 <iframe src="https://lichess.org/tv/frame?theme=blue3&bg=dark"
